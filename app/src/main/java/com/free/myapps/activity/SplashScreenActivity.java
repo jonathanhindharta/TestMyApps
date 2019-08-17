7faf1dba2f38c2b1f2057b0fc038ba2981bf403c
jonathanhindharta <jonathanhindharta@gmail.com>
2019-08-17 19:38:49 +0700

    Update SplashScreenActivity.java
    change splash screen change method to handler

--- app/src/main/java/com/free/myapps/activity/SplashScreenActivity.java
+++ app/src/main/java/com/free/myapps/activity/SplashScreenActivity.java
@@ -19,10 +19,10 @@ public class SplashScreenActivity extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent i = new Intent(SplashScreenActivity.this, LoginActivity.class);
+                    Intent i = new Intent(SplashScreenActivity.this, LoginActivity.class);
                     finish();
                     startActivity(i);
             }
-        }, 3000);
+        }, splashTime);
     }
 }
